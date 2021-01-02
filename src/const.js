06086c8a665e23b8e3de9b3b2c72e668896033ca
Vladislav <vlad74rus.rus@gmail.com>
2021-01-02 12:17:11 +0500

    chore: added filter presenter and modified filter view for processing

--- src/const.js
+++ src/const.js
@@ -4,5 +4,25 @@ export const SortTypes = {
   BY_RATING: `rating`
 };
 
+export const UserAction = {
+  UPDATE_FILM: `UPDATE_FILM`,
+  ADD_COMMENT: `ADD_COMMENT`,
+  DELETE_COMMENT: `DELETE_COMMENT`,
+};
+
+export const FilterType = {
+  ALL: `all`,
+  WATCHLIST: `watchlist`,
+  HISTORY: `history`,
+  FAVORITES: `favorites`,
+};
+
+export const UpdateType = {
+  PATCH: `PATCH`,
+  MINOR: `MINOR`,
+  MAJOR: `MAJOR`,
+  INIT: `INIT`
+};
+
 export const FILM_RELEASE_DATE_FORMAT = `D MMMM YYYY`;
 export const FILM_RUNTIME_FORMAT = `H[h] m[m]`;
