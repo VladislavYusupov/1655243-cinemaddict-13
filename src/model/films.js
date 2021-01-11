844ebbb46b096d1ab036000354fa610b8add6bf8
Vladislav <vlad74rus.rus@gmail.com>
2021-01-11 22:17:38 +0500

    chore: added api and adapter for films

--- src/model/films.js
+++ src/model/films.js
@@ -29,4 +29,47 @@ export default class Films extends Observer {
 
     this._notify(updateType, update);
   }
+
+  static adaptToClient(film) {
+    const filmInfo = film.film_info;
+    const userDetails = film.user_details;
+    const {id, comments} = film;
+    const {title, poster, director, writers, actors, release, runtime, genre, description} = filmInfo;
+    const {watchlist, favorite} = userDetails;
+
+    const adaptedFilm = {
+      id,
+      comments,
+      title,
+      alternativeTitle: filmInfo.alternative_title,
+      totalRating: filmInfo.total_rating,
+      poster,
+      ageRating: filmInfo.age_rating,
+      director,
+      writers,
+      actors,
+      releaseDate: release.date !== null ? new Date(release.date) : release.date,
+      country: release.release_country,
+      runtime,
+      genre,
+      description,
+      watchList: watchlist,
+      alreadyWatched: userDetails.already_watched,
+      watchingDate: userDetails.watching_date !== null ? new Date(userDetails.watching_date) : userDetails.watching_date,
+      favorite
+    };
+
+    return adaptedFilm;
+  }
+
+  static adaptToServer(film) {
+    const adaptedFilm = {
+      "watchlist": film.watchList,
+      "already_watched": film.alreadyWatched,
+      "watching_date": film.watchingDate instanceof Date ? film.watchingDate.toISOString() : null,
+      "favorite": film.favorite
+    };
+
+    return adaptedFilm;
+  }
 }
